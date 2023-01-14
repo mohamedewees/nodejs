b54af7d404a4ade27398dae9e03363b3f428f207
engmohamedewees@gmail.com <engmohamedewees@gmail.com>
2023-01-14 10:49:37 +0200

    js new projects v1.0.0

---
+++ forex/index.js
@@ -0,0 +1,12 @@
+var request = require('request');
+var options = {
+  'method': 'GET',
+  'url': 'https://api.fastforex.io/fetch-multi?from=USD&to=EGP&api_key=70f4c9704c-d098902118-rogvjq',
+  'headers': {
+    'Authorization': 'Basic ZXdlZXNfZXdlZXM6aGhXODB4UDQzUg=='
+  }
+};
+request(options, function (error, response) {
+  if (error) throw new Error(error);
+  console.log(response.body);
+});
